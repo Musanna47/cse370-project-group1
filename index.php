24d335cb734b66c497e757cf60dd381fadbd3adc
Muhtasim Hossain Musanna <muhtasim.hossain.musanna@g.bracu.ac.bd>
2023-12-05 00:10:01 +0600

    new initial commit

--- index.php
+++ index.php
@@ -5,63 +5,47 @@
 
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body class="login-body">
-    <header>
-        <nav>
-            <ul>
-                <li><a href="http://localhost/cse370-project-group1">HOME</a></li>
-                <li><a href="http://localhost/cse370-project-group1">SETTINGS</a></li>
-                <li><a href="http://localhost/cse370-project-group1">ABOUT</a></li>
-                <li><a href="http://localhost/cse370-project-group1">CONTACT</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <div class="center-container login">
-            <h1>Login</h1>
-            <form action="" method="post" class="center-container-inner">
-                <label for="username">Username<br>
-                    <input type="text" name="username" required>
-                </label><br>
-                <label for="password">Password<br>
-                    <input type="password" name="password" required>
-                </label><br>
-                <input type="submit" name="submit" value="Submit">
-            </form>
-        </div>
-    </main>
-    <footer>
-        <div>Made by CSE370 Group 1 - Fall 2023</div>
-    </footer>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Homepage</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Hedvig%20Letters%20Serif">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fira%20Code">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="logo-block">
+                    <img class="logo" src="images/food-delivery.png" alt="Logo">
+                    <h1>BiteBlitz</h1>
+                </div>
+                <div class="link-block">
+                    <a href="">ITEMS</a>
+                    <a href="">RESTAURANTS</a>
+                    <a href="">ABOUT</a>
+                    <a href="">CONTACT</a>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <img class="rectangle-background" src="images/red-white-rectangle.png" alt="Rectangle">
+            <div class="container">
+                <div class="container-left">
+                    <p>you <span class="yellow-color">hit</span> order we<br>
+                        do the <span class="yellow-color">running!</span></p><br>
+                </div>
+                <div class="container-right">
+                    <p>HELO</p>
+                </div>
+            </div>
+        </main>
+    </body>
 </html>
 
+
 <?php
-    if (isset($_POST["submit"])) {
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-        $query = "SELECT * FROM user WHERE username = '$username'";
-        try {
-            $result = mysqli_query($conn, $query);
-        } catch (mysqli_sql_exception $ex) {
-            exit("Error: ".$ex->getMessage());
-        }
-        if (is_null($row = mysqli_fetch_assoc($result))) {
-            echo "<div class='message'>Username does not exist in the database!<br></div>";
-            unset($_POST);
-        } elseif ($password != $row["password"]) {
-            echo "<div class='message'>Wrong password!<br></div>";
-            unset($_POST);
-        } else {
-            header("Location: logged-in.php");
-            exit();
-        }
-    }
+
+?>
--- mysql/dbcreate.sql
+++ mysql/dbcreate.sql
@@ -1,166 +1,174 @@
-CREATE TABLE area (  
-    area VARCHAR(30),  
-    PRIMARY KEY (area)  
+CREATE TABLE area (
+    area_id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    district VARCHAR(20) NOT NULL,
+    PRIMARY KEY (area_id)
 );
 
-CREATE TABLE address (  
-    address_id INT AUTO_INCREMENT,  
-    flat_no VARCHAR(6),  
-    house_no INT,  
-    road_no VARCHAR(5),  
-    zip_code CHAR(4),  
-    city VARCHAR(20),  
-    district VARCHAR(20),  
-    division VARCHAR(20),  
-    area VARCHAR(30),  
-    PRIMARY KEY (address_id),  
-    FOREIGN KEY (area) REFERENCES area (area) 
+CREATE TABLE address (
+    address_id INT AUTO_INCREMENT,
+    flat_no VARCHAR(10),
+    house_no INT NOT NULL,
+    road_no VARCHAR(10) NOT NULL,
+    zip_code CHAR(4),
+    area_id INT NOT NULL,
+    PRIMARY KEY (address_id),
+    FOREIGN KEY (area_id) REFERENCES area (area_id)
 );
 
-CREATE TABLE restaurant (  
-    restaurant_id INT AUTO_INCREMENT,  
-    name VARCHAR(30),  
-    phone_no VARCHAR(15),  
-    address_id INT,  
-    PRIMARY KEY (restaurant_id),  
-    FOREIGN KEY (address_id) REFERENCES address (address_id)  
+CREATE TABLE restaurant (
+    restaurant_id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    phone_no VARCHAR(15) NOT NULL UNIQUE,
+    address_id INT NOT NULL,
+    PRIMARY KEY (restaurant_id),
+    FOREIGN KEY (address_id) REFERENCES address (address_id)
 );
 
 CREATE TABLE menu_item (
-    food_id INT AUTO_INCREMENT,  
-    name VARCHAR(30),
+    food_id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
     category VARCHAR(30),
-    cuisine VARCHAR(20),
-    price DOUBLE,
+    price DOUBLE NOT NULL,
     restaurant_id INT,
     PRIMARY KEY (food_id),
-    FOREIGN KEY (restaurant_id) REFERENCES restaurant (restaurant_id)  
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant (restaurant_id) ON DELETE SET NULL
 );
 
-CREATE TABLE user (  
-    user_id INT AUTO_INCREMENT,  
-    name VARCHAR(30),  
-    phone VARCHAR(15),  
-    email VARCHAR(30),  
-    date_of_birth DATE,  
-    customer_flag BOOLEAN,  
-    nid_id VARCHAR(15),  
-    username VARCHAR(30),  
-    password VARCHAR(30),  
-    address_id INT,  
-    PRIMARY KEY (user_id),  
-    FOREIGN KEY (address_id) REFERENCES address (address_id)  
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    phone VARCHAR(15) UNIQUE,
+    email VARCHAR(30) NOT NULL UNIQUE,
+    date_of_birth DATE NOT NULL,
+    customer_flag BOOLEAN NOT NULL,
+    nid_no VARCHAR(15) UNIQUE,
+    username VARCHAR(30) NOT NULL UNIQUE,
+    user_password VARCHAR(30) NOT NULL,
+    due_amount INT NOT NULL DEFAULT 0,
+    address_id INT NOT NULL,
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (address_id) REFERENCES address (address_id)
 );
 
-CREATE TABLE delivery_locations (  
-    restaurant_id INT,  
-    area VARCHAR(30),  
-    PRIMARY KEY (restaurant_id, area),  
-    FOREIGN KEY (restaurant_id) REFERENCES restaurant (restaurant_id),  
-    FOREIGN KEY (area) REFERENCES area (area)  
+CREATE TABLE delivery_locations (
+    restaurant_id INT,
+    area_id INT,
+    PRIMARY KEY (restaurant_id, area_id),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant (restaurant_id) ON DELETE CASCADE,
+    FOREIGN KEY (area_id) REFERENCES area (area_id) ON DELETE CASCADE
 );
 
-CREATE TABLE cart (  
-    user_id INT,  
-    food_id INT,  
-    quantity INT,  
-    PRIMARY KEY (user_id, food_id),  
-    FOREIGN KEY (user_id) REFERENCES user (user_id),  
-    FOREIGN KEY (food_id) REFERENCES menu_item (food_id)  
+CREATE TABLE cart (
+    user_id INT,
+    food_id INT,
+    quantity INT NOT NULL,
+    PRIMARY KEY (user_id, food_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (food_id) REFERENCES menu_item (food_id) ON DELETE CASCADE
 );
 
-CREATE TABLE discount (  
-    campaign_id INT AUTO_INCREMENT,  
-    campaign_name VARCHAR(30),  
-    food_id INT,  
-    percentage DOUBLE,  
-    start_date DATE,  
-    expiry_date DATE,  
-    PRIMARY KEY (campaign_id, food_id),  
-    FOREIGN KEY (food_id) REFERENCES menu_item (food_id)  
+CREATE TABLE discount (
+    campaign_id INT AUTO_INCREMENT,
+    campaign_name VARCHAR(30) NOT NULL,
+    food_id INT,
+    percentage DECIMAL(5, 2) NOT NULL,
+    start_date DATE NOT NULL,
+    expiry_date DATE NOT NULL,
+    PRIMARY KEY (campaign_id, food_id),
+    FOREIGN KEY (food_id) REFERENCES menu_item (food_id) ON DELETE CASCADE
 );
 
-CREATE TABLE voucher (  
-    voucher_id INT AUTO_INCREMENT,  
-    promo_code VARCHAR(20),  
-    percentage DOUBLE,  
-    start_date DATE,  
-    expiry_date DATE,  
-    PRIMARY KEY (voucher_id)  
+CREATE TABLE voucher (
+    voucher_id INT AUTO_INCREMENT,
+    promo_code VARCHAR(20) NOT NULL UNIQUE,
+    percentage DECIMAL(5, 2) NOT NULL,
+    start_date DATE NOT NULL,
+    expiry_date DATE NOT NULL,
+    PRIMARY KEY (voucher_id)
 );
 
-CREATE TABLE voucher_used (  
+CREATE TABLE voucher_used (
     voucher_id INT,
     user_id INT,
     PRIMARY KEY (voucher_id, user_id),
-    FOREIGN KEY (user_id) REFERENCES user (user_id),  
-    FOREIGN KEY (voucher_id) REFERENCES voucher (voucher_id)  
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (voucher_id) REFERENCES voucher (voucher_id) ON DELETE CASCADE
 );
 
-CREATE TABLE rating (  
-    rating_id INT AUTO_INCREMENT,  
-    user_id INT,  
-    stars INT,  
-    comment VARCHAR(255),  
-    date DATE,  
-    restaurant_flag BOOLEAN,  
-    restaurant_id INT,  
-    food_id INT,  
-    PRIMARY KEY (rating_id),  
-    FOREIGN KEY (user_id) REFERENCES user (user_id),  
-    FOREIGN KEY (restaurant_id) REFERENCES restaurant (restaurant_id),  
-    FOREIGN KEY (food_id) REFERENCES menu_item (food_id)  
+CREATE TABLE rating (
+    rating_id INT AUTO_INCREMENT,
+    user_id INT,
+    stars INT NOT NULL,
+    comment VARCHAR(255),
+    date DATE NOT NULL,
+    restaurant_flag BOOLEAN NOT NULL,
+    restaurant_id INT,
+    food_id INT,
+    PRIMARY KEY (rating_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE SET NULL,
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant (restaurant_id) ON DELETE CASCADE,
+    FOREIGN KEY (food_id) REFERENCES menu_item (food_id) ON DELETE CASCADE
 );
 
-CREATE TABLE driver (  
-    driver_id INT AUTO_INCREMENT,  
-    name VARCHAR(30),  
-    phone VARCHAR(15),  
-    email VARCHAR(30),  
-    date_of_birth DATE,  
-    nid_id VARCHAR(15),  
-    license_no VARCHAR(20),  
-    address_id INT,  
-    PRIMARY KEY (driver_id),  
-    FOREIGN KEY (address_id) REFERENCES address (address_id)  
+CREATE TABLE driver (
+    driver_id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    phone VARCHAR(15) NOT NULL UNIQUE,
+    email VARCHAR(30) NOT NULL UNIQUE,
+    date_of_birth DATE NOT NULL,
+    nid_no VARCHAR(15) NOT NULL UNIQUE,
+    license_no VARCHAR(20) NOT NULL UNIQUE,
+    address_id INT NOT NULL,
+    PRIMARY KEY (driver_id),
+    FOREIGN KEY (address_id) REFERENCES address (address_id)
 );
 
-CREATE TABLE favorite (  
-    favorite_id INT AUTO_INCREMENT,  
-    name VARCHAR(30),  
-    food_id INT,  
-    quantity INT,  
-    user_id INT,  
-    PRIMARY KEY (favorite_id, food_id),  
-    FOREIGN KEY (food_id) REFERENCES menu_item (food_id),  
-    FOREIGN KEY (user_id) REFERENCES user (user_id)  
+CREATE TABLE favorites (
+    favorite_id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    food_id INT,
+    quantity INT NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY (favorite_id, food_id),
+    FOREIGN KEY (food_id) REFERENCES menu_item (food_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE payment (  
-    transaction_id INT AUTO_INCREMENT,  
-    user_id INT,  
-    payment_method BOOLEAN,  
-    transaction_source VARCHAR(30),  
-    amount INT,  
-    PRIMARY KEY (transaction_id),  
-    FOREIGN KEY (user_id) REFERENCES user (user_id)  
+CREATE TABLE payment (
+    transaction_id INT AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    payment_method VARCHAR(30),
+    transaction_source VARCHAR(30),
+    amount INT NOT NULL,
+    PRIMARY KEY (transaction_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE orders (  
-    order_id INT AUTO_INCREMENT,  
-    food_id INT,  
-    quantity INT,  
-    date DATE,  
-    delivery_status VARCHAR(15),  
-    payment_status VARCHAR(15),  
-    user_id INT,  
-    address_id INT,  
-    driver_id INT,  
-    voucher_id INT,  
-    PRIMARY KEY (order_id, food_id),  
-    FOREIGN KEY (food_id) REFERENCES menu_item (food_id),  
-    FOREIGN KEY (user_id) REFERENCES user (user_id),  
-    FOREIGN KEY (address_id) REFERENCES address (address_id),  
-    FOREIGN KEY (driver_id) REFERENCES driver (driver_id),  
-    FOREIGN KEY (voucher_id) REFERENCES voucher (voucher_id)  
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT,
+    date DATE NOT NULL,
+    delivery_status VARCHAR(15) NOT NULL,
+    total_price INT NOT NULL,
+    user_id INT,
+    address_id INT,
+    driver_id INT,
+    voucher_id INT,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE SET NULL,
+    FOREIGN KEY (address_id) REFERENCES address (address_id) ON DELETE SET NULL,
+    FOREIGN KEY (driver_id) REFERENCES driver (driver_id) ON DELETE SET NULL,
+    FOREIGN KEY (voucher_id) REFERENCES voucher (voucher_id) ON DELETE SET NULL
+);
+
+CREATE TABLE ordered_items (
+    order_id INT,
+    item_no INT,
+    food_id INT,
+    quantity INT NOT NULL,
+    price INT NOT NULL,
+    PRIMARY KEY (order_id, item_no),
+    FOREIGN KEY (food_id) REFERENCES menu_item (food_id) ON DELETE SET NULL,
+    FOREIGN KEY (order_id) REFERENCES orders (order_id) ON DELETE CASCADE
 );
